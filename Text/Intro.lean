b420f4e3e52652463e23fb1e83686683ba77ea7d
Thorsten Altenkirch <psztxa@exmail.nottingham.ac.uk>
2025-12-23 17:31:17 +0000

    fixed Text

---
+++ Text/Intro.lean
@@ -0,0 +1,14 @@
+import VersoManual
+import Content.Meta
+open Verso.Genre Manual
+open Verso.Genre.Manual.InlineLean
+open Verso.Code.External
+
+set_option verso.exampleProject "."
+set_option verso.exampleModule "Proofs.Intro"
+
+#doc (Manual) "Introduction" =>
+%%%
+tag := "intro"
+%%%
+These are lecture notes for COMP2012: Languages and Computation.
